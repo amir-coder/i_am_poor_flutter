df7ad6d957feb6fbdb99024f743b130aac974209
ayoub-kh <ja_khenfouf@esi.dz>
2020-08-08 23:40:20 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Center(child: Text('I Am Poor',)),
+      ),
+      body: Center(
+        child: Container(
+          child: Image.asset('assets/img/diamond.png')
+        ),  
+      ),
+    ),
+  ),);
+}
+
