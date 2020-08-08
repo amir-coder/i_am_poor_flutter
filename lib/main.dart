2615f4671ceacfa4ff789e6a305d84dd8e53901c
amirAlmamma <ja_amir@esi.dz>
2020-08-09 00:41:27 +0100

    corrected assets declaration

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.cyan,
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: Center(child: Text('I Am Poor',)),
-      ),
-      body: Center(
-        child: Container(
-          child: Image.asset('assets/img/diamond.png')
-        ),  
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(
+              child: Text(
+            'I Am Poor',
+          )),
+        ),
+        body: Center(
+          child: Container(child: Image.asset('assets/img/diamond.png')),
+        ),
       ),
     ),
-  ),);
+  );
 }
-
